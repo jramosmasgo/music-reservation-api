7ef6decdcce5736a73d155a03ce7254b3a687fe5
Jean Ramos <jeanramos@Mac-mini-de-Jean.local>
2022-05-27 12:56:32 -0500

    feat: add vercel link

--- src/app.ts
+++ src/app.ts
@@ -20,7 +20,7 @@ app.use(express.json({ limit: "50mb" }));
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", `${process.env.URL_FRONTEND}`],
   })
 );
 
